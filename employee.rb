37cf6488fd2735ece07fb22324e7ad9e61e237e8
Theo Obbard <theo@dali.dartmouth.edu>
2018-10-29 14:48:26 -0700

    Add #bonus to Employee/Manager classes

--- employee.rb
+++ employee.rb
@@ -1,15 +1,36 @@
 class Employee
+  
+  attr_reader :salary
+  
   def initialize(name, title, salary, boss)
     @name = name
     @title = title
     @salary = salary
     @boss = boss
   end
+  
+  def bonus(multiplier)
+    @bonus = @salary * multiplier
+  end
 end
 
 class Manager < Employee
-  def initialize(employees)
+  attr_reader :employees
+  
+  def initialize(name, title, salary, boss, employees)
     @employees = employees
-    super
+    super(name, title, salary, boss)
+  end
+  
+  def bonus(multiplier)
+    @bonus = 0
+    @employees.each do |emp|
+      @bonus += emp.salary
+    end
+    @bonus *= multiplier
   end
 end
+
+bob = Employee.new('bob', 'worker', 50000, 'Theo')
+theo = Manager.new('Theo', 'boss', 100000, 'Rikey', [bob])
+p theo.bonus(5)
