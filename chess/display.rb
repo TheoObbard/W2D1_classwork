270df61ae71e70b1426fa16b227e79587ae30561
Theo Obbard <theo@dali.dartmouth.edu>
2018-10-29 18:05:30 -0700

    Add #render to display.rb & update #move_piece in board.rb

--- chess/display.rb
+++ chess/display.rb
@@ -5,6 +5,25 @@ class Display
 
   def initialize(board)
     @cursor = Cursor.new([0,0], board)
+    @board = board 
   end 
+  
+  def move_cursor(next_pos)
+    @cursor.update_pos(next_pos) if @board.valid_pos?(next_pos)
+  end 
+  
+  def render
+    @board.grid.each do |row| 
+      # p row
+      row.each_with_index do |cell, ci|
+        if cell == @cursor.cursor_pos
+          print "yay".red
+        else
+          print "nay"
+        end
+      end
+    end
+    @cursor.get_input
+  end
   
 end 
